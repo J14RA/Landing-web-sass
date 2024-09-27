2ce183dd53b7b28a80515ba2ff7d78bb04d2ebee
Akbar <jihad.akbar.ar@gmail.com>
2024-09-27 16:39:28 +0700

    Fix the swiper and color

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import Swiper from "swiper";
 import "swiper/swiper-bundle.css";
 import "./styles/main.scss";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".swiper", {
+  var swiper1 = new Swiper(".swiper", {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
     slidesPerView: 3,
     spaceBetween: 30,
     centeredSlides: true,
@@ -13,5 +16,24 @@ document.addEventListener("DOMContentLoaded", () => {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    pagination: {
+      el: ".swiper-pagination", // Selector for pagination
+      clickable: true, // Allows pagination bullets to be clickable
+    },
+  });
+
+  var swiper2 = new Swiper(".partnership__list", {
+    modules: [Autoplay],
+    loop: true,
+    freeMode: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loopFillGroupWithBlank: true, // Adds blank slides to maintain the loop
+    cssMode: true,
+    slidesPerView: 5,
+    speed: 3000,
+    grabCursor: true,
   });
 });
